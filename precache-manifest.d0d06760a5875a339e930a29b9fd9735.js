293efe27645282b1fca385f1bc3c874c99a6c915
Surendra Sakkaravarthi <surendra.s@ad.infosys.com>
2019-10-15 18:12:14 +0530

    Updates

---
+++ precache-manifest.d0d06760a5875a339e930a29b9fd9735.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a07c9ee3119188300dbcb41f8f2134ee",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "9521bf6209e2c2564d25",
+    "url": "/my-app/static/css/main.89a59f18.chunk.css"
+  },
+  {
+    "revision": "ac1bf34bc4ee2e93799f",
+    "url": "/my-app/static/js/2.2ac32b09.chunk.js"
+  },
+  {
+    "revision": "9521bf6209e2c2564d25",
+    "url": "/my-app/static/js/main.1f992f92.chunk.js"
+  },
+  {
+    "revision": "9c47355d93f1cffa1d27",
+    "url": "/my-app/static/js/runtime~main.eed5ce82.js"
+  }
+]);
